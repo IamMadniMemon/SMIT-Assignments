cc9be6ffa0cb747b34146f6ef5ec1389ece21815
PHP/WordPress Developer <82670239+IamMadniMemon@users.noreply.github.com>
2025-12-27 20:12:55 +0500

    Update script.js

--- vowels-words/script.js
+++ vowels-words/script.js
@@ -1,1 +1,16 @@
+let getUsername = prompt('Please Enter Your Name');
+let arr = getUsername.split('');
+let vowelWords = ['a', 'e', 'i', 'o', 'u'];
+//console.log( arr );
 
+for (let i = 0; i < arr.length; i++) {
+    let magicNumber = i + 1;
+    let printing;
+    if (arr[i] == vowelWords[0] || arr[i] == vowelWords[1] || arr[i] == vowelWords[2] || arr[i] == vowelWords[3] || arr[i] == vowelWords[4]) {
+        //console.log(arr[i]);
+         printing = ` is vowel word`;
+    }else{
+        printing = ``;
+    }
+    console.log( arr[i] + printing );
+}
