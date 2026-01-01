cab4a69135245d7cc4fb4e52f2b36f3633fbee84
PHP/WordPress Developer <82670239+IamMadniMemon@users.noreply.github.com>
2026-01-02 03:30:46 +0500

    Update script.js

--- leap-year/script.js
+++ leap-year/script.js
@@ -4,11 +4,11 @@ let getYear = getComDate.getFullYear();
 // console.log( getYear );
 
 if( getYear % 400 == 0 ){
-    console.log( `${getYear} Leap Year` );
+    console.log( `${getYear} Leap Year is year main 366 days the` );
 }else if( getYear % 100 == 0 ){
-    console.log( `${getYear} Leap Year` );
+    console.log( `${getYear} Leap Year is year main 366 days the` );
 }else if( getYear % 4 == 0 ){
-    console.log( `${getYear} Leap Year` );
+    console.log( `${getYear} Leap Year is year main 366 days the` );
 }else{
     console.log( `${getYear} Leap Year Nae Hai` );
 }
