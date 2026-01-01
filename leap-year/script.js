770f630b9f51ff5a967492a4c3f7ddc2b061ecad
PHP/WordPress Developer <82670239+IamMadniMemon@users.noreply.github.com>
2026-01-02 03:27:19 +0500

    Update script.js

--- leap-year/script.js
+++ leap-year/script.js
@@ -1,1 +1,23 @@
+let getUsername = +prompt('Please Enter The Year');
+let getComDate = new Date( getUsername, 0, 1 );
+let getYear = getComDate.getFullYear();
+// console.log( getYear );
+
+if( getYear % 400 == 0 ){
+    console.log( `${getYear} Leap Year` );
+}else if( getYear % 100 == 0 ){
+    console.log( `${getYear} Leap Year` );
+}else if( getYear % 4 == 0 ){
+    console.log( `${getYear} Leap Year` );
+}else{
+    console.log( `${getYear} Leap Year Nae Hai` );
+}
+
+
+
+
+
+
+
+
 
